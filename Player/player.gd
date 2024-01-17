53a7bdccf3696e3d29ed7dbcfcbb7c391cd5f1fe
Tony Kanyamuka <tonykanyamuka@gmail.com>
2024-01-17 16:35:52 +0200

    create Player Controller

---
+++ Player/player.gd
@@ -0,0 +1,51 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+@onready var animatedSrite= $AnimatedSprite2D
+@onready var anim = $AnimationPlayer
+
+
+func _ready():
+	pass
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+	
+		anim.play("Jump")
+		
+		
+	if velocity.y > 0:
+		anim.play("Fall")      
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("ui_left", "ui_right")
+	
+	if direction == -1:
+		animatedSrite.flip_h = true
+	elif direction ==1:
+		animatedSrite.flip_h = false
+	if direction:
+		velocity.x = direction * SPEED
+		#if velocity.y == 0:
+		if is_on_floor_only():
+			anim.play("Run")
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		#if velocity.y == 0:
+		if is_on_floor_only():
+			anim.play("Idle")
+	#if velocity.y > 0:
+		#anim.play("Fall")  
+
+	move_and_slide()
